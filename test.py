4b48f12d4d20b4106c2529d5fc71b40bae89b8c2
peymankor <peyman.kor@uis.no>
2022-03-16 17:16:40 +0100

    New file

---
+++ test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+testdata = np.array([1,2,3])
+testdata
+
+testdatav2 = np.array([1,2,3])
+testdatav2
+
+
+def DoStuff():
+    testdataLocalChangesDontCommit = np.array([1,2,3])
+    testdataLocalChangesDontCommit
+
+anotherChangeIForgotv222=100
+testdata33 = 33
+testdata33
