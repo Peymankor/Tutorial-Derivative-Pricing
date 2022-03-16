30a2c6c6d3de05e1a6578cdd6fa705e9b330e1aa
peymankor <peyman.kor@uis.no>
2022-03-16 17:01:44 +0100

    Added new changes

--- test.py
+++ test.py
@@ -3,3 +3,15 @@ import numpy as np
 
 testdata = np.array([1,2,3])
 testdata
+
+testdatav2 = np.array([1,2,3])
+testdatav2
+
+
+def DoStuff():
+    testdataLocalChangesDontCommit = np.array([1,2,3])
+    testdataLocalChangesDontCommit
+
+
+testdata33 = 33
+testdata33
