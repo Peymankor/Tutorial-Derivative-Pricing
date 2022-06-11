c1ac882f71c0ff96d04960232c2e0c8a6f18f0da
peyman <peyman.kor@uis.no>
2022-06-11 20:12:03 +0200

    binomial method in datclass format

--- cont_curve_methods.py
+++ cont_curve_methods.py
@@ -48,6 +48,115 @@ def binomial_put(S, K, T, r, vol, N):
 
 #########################################################
 
+#paths: np.ndarray = np.empty([num_paths, self.num_steps + 1])
+
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class BinPolicy:
+
+    spot_price: float
+    strike: float
+    expiry: float
+    rate: float
+    vol: float
+    num_steps: int
+
+    def option_price(self) -> float:
+
+        # Number of time steps
+        dt = self.expiry/self.num_steps
+    
+        # u value 
+        u =  np.exp(self.vol * np.sqrt(dt))
+    
+        # 1/u value
+        d = 1/u
+    
+        # probability of q 
+        q = (np.exp(self.rate * dt) - d)/(u - d)
+    
+        # probability of q
+    
+        # 
+        C = {}
+        #price_array = []
+        
+        #future_value_list = []
+    
+        #payoff function for put options    
+        for m in range(0, self.num_steps+1):
+
+            C[(self.num_steps, m)] = max(self.strike - 
+                self.spot_price * (u ** (2*m - self.num_steps)), 0) 
+    
+    
+        for k in range(self.num_steps-1, -1, -1):
+
+            for m in range(0,k+1):
+
+                future_value = np.exp(-self.rate * dt) * (q * C[(k+1, m+1)] + 
+                                (1-q) * C[(k+1, m)])
+                exercise_value =  max(self.strike - self.spot_price * (u ** (2*m-k)),0)
+            
+
+                C[(k, m)] = max(future_value, exercise_value)
+    
+        return C[(0,0)]
+
+
+    def continuation_value_list(self):
+
+        # Number of time steps
+        dt = self.expiry/self.num_steps
+    
+        # u value 
+        u =  np.exp(self.vol * np.sqrt(dt))
+    
+        # 1/u value
+        d = 1/u
+    
+        # probability of q 
+        q = (np.exp(self.rate * dt) - d)/(u - d)
+    
+        # probability of q
+    
+        # 
+        C = {}
+        #price_array = []
+        
+        future_value_list = []
+    
+        #payoff function for put options    
+        for m in range(0, self.num_steps+1):
+
+            C[(self.num_steps, m)] = max(self.strike - 
+                self.spot_price * (u ** (2*m - self.num_steps)), 0) 
+    
+    
+        for k in range(self.num_steps-1, -1, -1):
+
+            for m in range(0,k+1):
+
+                exercise_value =  max(self.strike - self.spot_price * (u ** (2*m-k)),0)
+
+                future_value = np.exp(-self.rate * dt) * (q * C[(k+1, m+1)] + (1-q) * C[(k+1, m)])
+            
+                future_value_list.append((k,self.spot_price* (u ** (2*m-k)),future_value))
+
+                C[(k, m)] = max(future_value, exercise_value)
+        
+        return future_value_list
+
+
+
+import numpy as np
+test = BinPolicy(spot_price=36, strike=40, expiry=1,
+            rate=0.06, vol=0.2, num_steps=5)
+
+
+test.option_price()
+test.continuation_value_list()
 ####################### Q-Learning Method ####################
 
 def continuation_curve(
@@ -69,7 +178,7 @@ r_value = 0.06
 sd_value = 0.2
 T_value = 1
 #paths_value = 100000
-steps_value = 50
+steps_value = 5
 
 K_value = 40
 k_value = 4
@@ -77,6 +186,7 @@ k_value = 4
 strike = 40
 
 ddd , v=binomial_put(S=S0_value, K=K_value, T=T_value, r=r_value,
-                vol=sd_value, N=10)
+                vol=sd_value, N=5)
 
 ddd
+v
