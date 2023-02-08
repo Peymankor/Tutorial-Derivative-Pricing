d4604e4a59f42cb8042edec37790823c1b6d91cd
peyman <peyman.kor@uis.no>
2023-02-08 14:35:16 +0100

    dicussing on lsm boundary

--- lsmfrommodule.py
+++ lsmfrommodule.py
@@ -16,14 +16,14 @@ pretty.install()
 
 import matplotlib.pyplot as plt
 
-from lsmfresh import lsm_policy
+from lsmfresh import lsm_policy, option_price_lsm
 
 S0_value = 36
 r_value = 0.06
 sd_value = 0.2
 T_value = 1
-paths_value = 100000
-steps_value = 50
+paths_value = 100
+steps_value = 5
 
 K_value = 40
 k_value = 4
@@ -41,8 +41,10 @@ Beta_list1 = lsm_policy(S0=S0_value,
 
 
 
+Stock_Matrix_GBM
 
 
+Beta_list1
 
 
 num_steps = 48
@@ -52,18 +54,34 @@ expiry = 1
 strike = 40
 prices: np.ndarray = np.arange(0., strike + 0.1, 0.1)
 prices
-num_steps = 48
+num_steps = 49
 expiry = 1
 
 x = []
 y = []
 
-for step in range(2,num_steps,1):
+Stock_Matrix_GBM_test = SimulateGBM(S0=S0_value, r=r_value, sd=sd_value, T=T_value, 
+paths=1000,steps=steps_value, reduce_variance=True)
+
+Stock_Matrix_GBM_test
+
+def payoff_func(_: float, s: float) -> float:
+         return max(strike - s, 0.)
+
+option_price_lsm(scoring_data=Stock_Matrix_GBM_test, 
+num_steps=steps_value-1,payoff= payoff_func, rate=r_value, expiry=T_value, 
+Beta_list=Beta_list1, k=k_value)
+Beta_list1
+for step in range(steps_value):
     
     print(step)
     cp = np.array([np.dot(laguerre_polynomials_ind(p,k=4), 
                 Beta_list1[step]) for p in prices])
 
+    print(cp)
+    plt.plot(prices,cp)
+    #plt.plot(prices,strike-prices)
+    #plt.show()
     ep = np.array([max(strike - p, 0) for p in prices])
 
 
@@ -74,6 +92,8 @@ for step in range(2,num_steps,1):
         x.append(step * expiry / num_steps)
         y.append(max(ll))
 
+plt.plot(prices,strike-prices, "--")
+plt.show()
 final: Sequence[Tuple[float, float]] = \
         [(p, max(strike - p, 0)) for p in prices]
 x.append(expiry)
